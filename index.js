67d4a376ae4307558bd40372726c566ec4f787e8
Uri32 <132776580+Uri32@users.noreply.github.com>
2023-05-13 22:22:32 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let munheco = document.getElementById("munheco");
+
+
+    let textoCifrado = texto
+      .replace(/e/gi,"enter")
+      .replace(/i/gi,"imes")
+      .replace(/a/gi,"ai")
+      .replace(/o/gi,"ober")
+      .replace(/u/gi,"ufat");
+     
+       if(texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con exito";
+        parrafo.textContent= "";
+        munheco.src = "./img/encriptado.jpg";
+       }else{
+        munheco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningun mensaje encontrado";
+        parrafo.textContent= "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar al menos un texto");
+
+       }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let munheco = document.getElementById("munheco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi,"e")
+    .replace(/imes/gi,"i")
+    .replace(/ai/gi,"a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      munheco.src = "./img/desencriptado.jpg";
+    } else {
+      munheco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Debes ingresar al menos un texto");
+    }
+}
